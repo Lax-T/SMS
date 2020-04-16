b8348605f46855e3fb7f9dd7abeced310d63842b
Lax-T <laxtec@gmail.com>
2020-04-17 01:47:59 +0300

    Extended GLIB. Added PrintF function, minus icon for font 44px

--- inc/ui/glib.h
+++ inc/ui/glib.h
@@ -10,3 +10,4 @@ void gl_setCursor(u8 x, u8 y);
 void gl_mergeTile(u8 tile[]);
 void gl_printString(u8 *string, u8 font_id);
 void gl_drawHLine (u8 line_width, u8 line_len);
+void gl_printFString(char *string, void *val, u8 font_id);
