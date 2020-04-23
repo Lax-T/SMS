df93d65907635f6107223a5f7d5deb6216de01d5
Lax-T <laxtec@gmail.com>
2020-04-24 01:39:19 +0300

    Added slow I2C bus driver.

--- inc/init.h
+++ inc/init.h
@@ -5,5 +5,17 @@
 #define RCC_HSE_FAIL 0x01
 #define RCC_PLL_FAIL 0x02
 
+/* IO definitions */
+/* Slow I2C bus */
+#define SLI2C_SCL_PIN GPIO_Pin_9
+#define SLI2C_SCL_PORT GPIOA
+#define SLI2C_SDA_PIN GPIO_Pin_10
+#define SLI2C_SDA_PORT GPIOA
+
+/* DS3231 square waveform output (SQW) (MCU in) */
+#define SQW_IN_PIN GPIO_Pin_11
+#define SQW_IN_PORT GPIOA
+
+/* Functions */
 void init();
 unsigned char clkInit();
