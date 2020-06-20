7e94defc7215c3578b8effd349e1649d6be62bad
Lax-T <laxtec@gmail.com>
2020-06-21 01:57:03 +0300

    Extended datetime lib, added new calculation methods.

--- src/misc/datetime.c
+++ src/misc/datetime.c
@@ -21,3 +21,66 @@ void deSerializeDateTime(u8 buffer[], struct DateTime *date_time) {
 	date_time->month = buffer[5];
 	date_time->year = buffer[6];
 }
+
+/* Check and return 1 (True) if year is leap. Starting from 2000 (assumed as 0) */
+u8 dt_isLeapYear(u8 year) {
+	if (year == 0) {
+		return TRUE;
+	}
+	if ((year % 4) == 0 && year != 100) { // 2100 is not a leap year
+		return TRUE;
+	}
+	return FALSE;
+}
+
+/* Days in month lookup array */
+const u8 _dt_days_in_month_lookup[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+/* Return number of days in particular year/month */
+u8 dt_getDaysInMonth(u8 year, u8 month) {
+	if (month == 2) {
+		if (dt_isLeapYear(year) == TRUE) {
+			return 29;
+		} else {
+			return 28;
+		}
+	}
+	return _dt_days_in_month_lookup[month - 1];
+}
+
+/* Calculate day of week for a given date */
+u8 dt_getDayOfWeek(u8 year, u8 month, u8 day) {
+	u16 total_days = 0;
+	u8 i;
+	/* If year less than 100 */
+	if (year < 100) {
+		/* Account days in years */
+		if (year > 0) {
+			total_days += ((year - 1) / 4) + 1; // +1 because 0 year is leap
+		}
+		total_days += 365 * year;
+		/* Account days in months */
+		for (i=1; i<month; i++) {
+			total_days += dt_getDaysInMonth(year, i);
+		}
+		/* Account days in current month */
+		total_days += day + 4; // +4 because 01.01.2000 starts from Saturday (first DoW offset)
+	}
+	/* If year 100 and higher */
+	else {
+		/* Account days in years */
+		if (year > 100) {
+			total_days += (year - 101) / 4;
+		}
+		total_days += 365 * (year - 100);
+		/* Account days in months */
+		for (i=1; i<month; i++) {
+			total_days += dt_getDaysInMonth(year, i);
+		}
+		/* Account days in current month */
+		total_days += day + 3; // +3 because 01.01.2100 starts from Friday (first DoW offset)
+	}
+	return (total_days % 7) + 1;
+}
+
+
+
